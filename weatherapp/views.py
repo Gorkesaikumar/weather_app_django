ec918bbe11b862a9a2c2887e69f61f006c144cdb
saikumar <gorkesaikumar@gmail.com>
2025-11-07 10:52:29 +0530

    initial commit

--- weatherapp/views.py
+++ weatherapp/views.py
@@ -1,57 +1,44 @@
 from django.shortcuts import render
 from django.contrib import messages
+from decouple import config
 import requests
 import datetime
 
-
 def home(request):
-   
-    if 'city' in request.POST:
-         city = request.POST['city']
-    else:
-         city = 'indore'     
-    
-    url = f'https://api.openweathermap.org/data/2.5/weather?q={city}&appid='
-    PARAMS = {'units':'metric'}
+    city = request.POST.get('city', 'indore')
 
-    API_KEY =  ''
-
-    SEARCH_ENGINE_ID = ''
-     
-    query = city + " 1920x1080"
-    page = 1
-    start = (page - 1) * 10 + 1
-    searchType = 'image'
-    city_url = f"https://www.googleapis.com/customsearch/v1?key={API_KEY}&cx={SEARCH_ENGINE_ID}&q={query}&start={start}&searchType={searchType}&imgSize=xlarge"
-
-    data = requests.get(city_url).json()
-    count = 1
-    search_items = data.get("items")
-    image_url = search_items[1]['link']
-    
+    API_KEY = config('OPENWEATHER_KEY')
+    url = f'https://api.openweathermap.org/data/2.5/weather?q={city}&appid={API_KEY}'
+    params = {'units': 'metric'}
 
     try:
-          
-          data = requests.get(url,params=PARAMS).json()
-          description = data['weather'][0]['description']
-          icon = data['weather'][0]['icon']
-          temp = data['main']['temp']
-          day = datetime.date.today()
+        data = requests.get(url, params=params).json()
 
-          return render(request,'weatherapp/index.html' , {'description':description , 'icon':icon ,'temp':temp , 'day':day , 'city':city , 'exception_occurred':False ,'image_url':image_url})
-    
-    except KeyError:
-          exception_occurred = True
-          messages.error(request,'Entered data is not available to API')   
-          # city = 'indore'
-          # data = requests.get(url,params=PARAMS).json()
-          
-          # description = data['weather'][0]['description']
-          # icon = data['weather'][0]['icon']
-          # temp = data['main']['temp']
-          day = datetime.date.today()
+        # if city not found
+        if data.get("cod") != 200:
+            raise ValueError("Invalid city")
 
-          return render(request,'weatherapp/index.html' ,{'description':'clear sky', 'icon':'01d'  ,'temp':25 , 'day':day , 'city':'indore' , 'exception_occurred':exception_occurred } )
-               
-    
-    
+        context = {
+            'description': data['weather'][0]['description'],
+            'icon': data['weather'][0]['icon'],
+            'temp': data['main']['temp'],
+            'day': datetime.date.today(),
+            'city': city,
+            'exception_occurred': False,
+            'image_url': 'https://images.pexels.com/photos/1107717/pexels-photo-1107717.jpeg?auto=compress&cs=tinysrgb&w=1600'
+        }
+
+    except Exception:
+        messages.error(request, 'City not found')
+
+        context = {
+            'description': 'clear sky',
+            'icon': '01d',
+            'temp': 25,
+            'day': datetime.date.today(),
+            'city': 'indore',
+            'exception_occurred': True,
+            'image_url': 'https://images.pexels.com/photos/3008509/pexels-photo-3008509.jpeg?auto=compress&cs=tinysrgb&w=1600'
+        }
+
+    return render(request, 'weatherapp/index.html', context)
